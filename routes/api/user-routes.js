3b09d5b200649e75e8830036f317bb0b1f765ec0
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-07-15 13:36:24 -0500

    added user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/user-controller');
+
+// /api/Users
+router
+  .route('/')
+  .get(getAllUser)
+  .post(createUser);
+
+// /api/Users/:id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+  router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
